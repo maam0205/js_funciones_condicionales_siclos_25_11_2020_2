6a776138d0eb49197de27989c8b11bfd382667fc
unknown <maalma0205@gmail.com>
2020-11-25 21:18:17 -0500

    js_funciones_condiciones_siclos_25_11_2020_2

---
+++ main.js
@@ -0,0 +1,126 @@
+/*let edad=prompt("Ingrese su edad");
+
+if(edad>=18){
+    alert("Usted es mayor de edad");
+}
+else{
+    alert("Usted es menor de edad");
+}*/
+
+
+/*let tarjeta=true;
+
+if(tarjeta!=true){
+    alert("Entro por la puerta grande");
+}
+else{
+    alert("Entro por la puerta pequeña");
+}*/
+
+
+
+/*let numero="30";
+
+if(numero===30){
+    alert("Puede entrar");   
+}
+else{
+    alert("No puede entrar");
+}*/
+
+
+/*let tarjeta = true;
+
+if(tarjeta !== false){
+    alert("Entro por la puerta grande");
+}
+else{
+    alert("Entro por la puerta pequeña");
+}*/
+
+
+
+
+/*let numero = prompt("Ingrese un numero mayor que (20)");
+
+if(numero >= 20){
+    alert("El numero ingresado es : "+numero+" es mayor que 20");
+}
+else{
+    alert("El numero no es correcto");
+}*/
+
+/*let numero = prompt("Ingrese un numero menor que (20)");
+
+if(numero < 20){
+    alert("El numero ingresado es : "+numero+" es menor que 20");
+}
+else{
+    alert("El numero no es correcto");
+}*/
+
+
+//OPERADORES CONDICION AND
+/*let numero_a = false;
+let numero_b = false;
+
+if(numero_a &= numero_b){
+    alert(true);
+}
+else{
+    alert(false);
+}*/
+
+//OPERADORES CONDICION 0R
+/*let numero_a = false;
+let numero_b = false;
+
+if(numero_a |= numero_b){
+    alert(true);
+}
+else{
+    alert(false);
+}*/
+
+
+//OPERADORES CONDICION XOR
+/*let numero_a = true;
+let numero_b = false;
+
+if(numero_a ^= numero_b){
+    alert(true);
+}
+else{
+    alert(false);
+}*/
+
+
+/*let edad = prompt("Digite su edad");
+
+if(edad == 18){
+    alert("Cumplio los 18 y es mayor de edad");
+}
+else if(edad > 18){
+    alert("Es mayor de edad");
+}
+else{
+    alert("Es menor de edad");
+}*/
+
+
+/*let edad = prompt("Digite su edad");
+
+if(edad == 18){
+    alert("Cumplio los 18 y es mayor de edad");
+}
+            //true    &&    //true
+else if( (edad >= 19) && (edad <= 20) ){
+    alert("Es mayor de edad y es mayor a 20");
+}
+else{
+    alert("Es menor de edad");
+}*/
+
+
+
+
